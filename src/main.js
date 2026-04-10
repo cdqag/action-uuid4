35366f7a5aaf2371ebb0ca131f14116e6d1c6168
Marek Sierociński <2142811+marverix@users.noreply.github.com>
2026-04-10 11:21:12 +0200

    fix: Use GITHUB_OUTPUT

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+import fs from 'node:fs'
 import uuid from '@lukeed/uuid';
 
 // Note: I'm not using @actions/core here, because there is no point to install and embed
 // such big library only for one simple command. It's an overkill.
 
+function debug(msg) {
+    console.log(`::debug::${msg}`);
+}
+
+function setOutput(name, value) {
+    // https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/
+    fs.writeFileSync(process.env['GITHUB_OUTPUT'], `${name}=${value}`, { flag: 'a' });
+}
+
 const theUuid = uuid();
-console.log(`::debug::UUID v4 generated: ${theUuid}`)
-console.log(`::set-output name=uuid::${theUuid}`);
+debug(`UUID v4 generated: ${theUuid}`)
+setOutput('uuid', theUuid);
